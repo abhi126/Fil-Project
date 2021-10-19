639d0f48ecad4d962f4597c8211f73b8204aa235
abhishek3758 <singlaabhishek84@gmail.com>
2021-10-19 18:38:11 +0530

    first commit

---
+++ home/prediction_mode1.py
@@ -0,0 +1,11 @@
+# # 'USD Coin':['usd','model_usd'],
+# #                 'Solana':['solana','model_solana']
+
+# 'Bitcoin':['data_bitc','model'],
+#                 'Ethereum':['ethc','model_ether'],
+#                 'Binance Coin':['binance','model_binance'],
+#                 'Tether':['tether','model_tether'],
+#                 'Cardano':['cardano','model_cardano'],
+#                 'XRP':['xrp','model_xrp'],
+#                 'Dogecoin':['doge','model_doge'],
+#                 'Polkadot':['polka','model_polka'],
